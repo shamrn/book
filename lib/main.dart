946c80a4cecfb56dbd5fc97db9159c0f6fe0085b
Vladislav Shamrin <shamrin2007@mail.ru>
2023-03-11 17:12:37 +0300

    Init project

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const GoodReaderApp());
+
+class GoodReaderApp extends StatelessWidget {
+  const GoodReaderApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
